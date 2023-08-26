70c405910293ab5f5a333fa214a7792dcdbb0da0
sams-ddev <sams.alxstudent@gmail.com>
2023-08-26 21:12:19 +0000

    writing script that usues variables

--- script_files/0x02-variables.sh
+++ script_files/0x02-variables.sh
@@ -3,5 +3,5 @@
 #the 'name' variable stores the name.
 name="Samuel Ashigbui Zerd"
 echo "Hello, $name!, are a software engineer?"
-read -p age #prompts user for input
-echo "You are $age years old."
+read -p "Enter your username: " user_name #prompts user for input
+echo "You entered $user_name"
