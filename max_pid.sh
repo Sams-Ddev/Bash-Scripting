dbeffaaecff1446e58f5d25cdecf92e6642edf06
sams-ddev <sams.alxstudent@gmail.com>
2023-09-12 01:11:46 +0000

    correction on script

--- max_pid.sh
+++ max_pid.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-maxi_pid = $(/proc/sys/kernel/pid_max)
-echo "The maximum pid value is: $(maxi_pid)"
+max_pid = $(/proc/sys/kernel/pid_max)
+echo "The maximum pid value is: $(max_pid)"
