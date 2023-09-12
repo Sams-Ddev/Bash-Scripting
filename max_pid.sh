7d6da75c63a9ed4537ebf5f27d91891f7920000f
sams-ddev <sams.alxstudent@gmail.com>
2023-09-12 01:32:43 +0000

    shell script runtime success

--- max_pid.sh
+++ max_pid.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
 
-max_pid = $(cat /proc/sys/kernel/pid_max)
-echo "The maximum pid value is: $max_pid"
+if [ -f /proc/sys/kernel/pid_max ]; then
+	max_pid=$(cat /proc/sys/kernel/pid_max)
+	echo "The maximum pid value  is: $max_pid"
+else
+	echo "The /proc/sys/kernel/pid_max cant br found"
+fi
